dd04548fa715abfb08f79f361d2f63cf0ea9f7b8
codemass <worklette@yandex.ru>
2020-03-30 00:57:33 +0300

    2st commit

---
+++ app/src/main/java/com/imdvlp/imdvlp/test_g/MainActivity.java
@@ -0,0 +1,13 @@
+package com.imdvlp.imdvlp.test_g;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);//2st commit
+    }
+}
